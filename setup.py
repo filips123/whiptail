6f80de9eecd893d7a39affa5d8c495585ecb22ba
filips <filip.stamcar@hotmail.com>
2016-11-13 12:18:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Operating System :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
