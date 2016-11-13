5468ce4da0bfc0b5f3b6aa363d769d8f26c23899
filips <filip.stamcar@hotmail.com>
2016-11-13 14:00:26 +0100

    Update whiptailPy

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-readme = read('README.rst')
-changes = read('CHANGES.txt')
-version_file = 'whiptail.py'
+try:
+    readme = read('README.rst')
+    changes = read('whiptailPy', 'CHANGES.txt')
+except:
+    readme = "Use whiptail to display dialog boxes from shell scripts"
+    changes = ""
 version = 1.0
 
 setup(
@@ -15,21 +18,12 @@ setup(
     description="Use whiptail to display dialog boxes from shell scripts",
     long_description=readme + '\n\n' + changes,
     keywords='whiptail',
-    author='Marwan Alsabbagh / filips',
-    author_email='marwan.alsabbagh@gmail.com / filip.stamcar@hotmail.com',
+    author='filips',
+    author_email='filip.stamcar@hotmail.com',
     url='https://github.com/fillips/whiptail',
-    license='BSD',
-    py_modules=['whiptail'],
-    namespace_packages=[],
-    include_package_data=True,
-    platforms='Linux',
+    py_modules=['whiptailPy'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
     ],
 )
